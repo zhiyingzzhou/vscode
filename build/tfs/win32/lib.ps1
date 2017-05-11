656b34af75ff43cf4917efd90a740598e32b395f
Joao Moreno <jomo@microsoft.com>
2017-05-11 14:10:52 +0200

    update build scripts

---
+++ build/tfs/win32/lib.ps1
@@ -0,0 +1,15 @@
+# stop when there's an error
+$ErrorActionPreference = 'Stop'
+
+# set agent specific npm cache
+if (Test-Path env:AGENT_WORKFOLDER) {
+	$env:npm_config_cache = "${env:AGENT_WORKFOLDER}\npm-cache"
+}
+
+# throw when a process exits with something other than 0
+function exec([scriptblock]$cmd, [string]$errorMessage = "Error executing command: " + $cmd) {
+    & $cmd
+    if ($LastExitCode -ne 0) {
+        throw $errorMessage
+    }
+}
